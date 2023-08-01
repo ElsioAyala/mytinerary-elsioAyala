283537a44d2ba20ffb5bb470c545c61653abb108
ElsioAyala <elsio_racing@hotmail.com>
2023-08-01 13:48:04 -0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <header className="l-container header">
+        <p className="logo">
+            My Tinerary
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="mainNavBar">
+          <var>
+            <a className="nav-link" href="http://">Home</a> 
+            <a className="nav-link" href="http://">Cities</a>
+          </var>
+          <button className="btn-login">
+            login
+          </button>
+        </div>
+      </header>
+      <main className="l-container main">
+        <div className="main-content">
+          <div className="hero">
+            <h2 className='hero-title'>Find the perfect destination</h2>
+            <p className='hero-description'>
+              Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.
+            </p>
+            <button className="hero-btn">
+              View More
+            </button>
+          </div>
+          <div className="hero-img">
+            <img src="/no-image-available.jpg" alt="imagen"/>
+          </div>
+          
+        </div>
+      </main>
+      <footer className="footer">
+
+      </footer>
     </>
   )
 }
