7a3faf3b04906c08bffd58c5483512a969a24d14
ElsioAyala <elsio_racing@hotmail.com>
2023-09-15 15:00:24 -0300

    Update cityQueries.js

--- src/services/cityQueries.js
+++ src/services/cityQueries.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mytinerary-back-ayalaelsio-dh0q-dev.fl0.io/",
+  baseURL: "https://mytinerary-back-ayalaelsio-dh0q-dev.fl0.io/api/",
 });
 
 export const getCities = async (queryParam = "") => {
